4218d2296c25176289659f653f1841f6132466d0
AVGAVGAVGAVG <gorshkov-av86@mail.ru>
2025-03-07 21:00:53 +0400

    сохранение 07.03.2025

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,37 @@ import { Route, BrowserRouter,Routes } from 'react-router-dom'
 import ProductDetail from './pages/ProductDetail'
 import CartPage from './pages/CartPage'
 import Home from './pages/Home'
+import CheckoutPage from "./pages/CheckoutPage"
+import PurchareHistory from "./pages/PurchareHistory"
 
 export default function App() {
   const [cartItems,setCartItems]=useState([])
 
   const addToCart = (product) =>{
-    setCartItems(prev=>[...prev,product])
+    setCartItems((prev)=>{
+      const existing= prev.find((item)=>item.id===product.id)
+      if (existing){
+        return prev.map((item)=>
+        item.id===product.id ? {  ...item,quantity:item.quantity + 1} :
+        item
+         )
+        }
+        return [...prev,{...product,quantity: 1 }]
+      })
   }
-  const removeFromCart =(id)=>{
-    setCartItems(prev=>prev.filter(item=>item.id !==id))
+  const handleUpdateQuantity = (id,newQuantity) =>{
+    setCartItems((prev)=>{
+      prev.map((item)=>{
+        item.id=== id ? {...item,quantity:newQuantity}:item
+      })
+    })
+  }
+
+  const handleRemoveItem =(id)=>{
+    setCartItems((prev)=>prev.filter((item)=>item.id !==id))
+  }
+  const clearCart = ()=>{
+    setCartItems([])
   }
   
 return (
@@ -27,7 +49,11 @@ return (
           <Route path="/product/:id"element={<ProductDetail addToCart=
           {addToCart}/>} />
           <Route path="/cart" element={<CartPage cartItems={cartItems} 
-          removeFromCart={removeFromCart} /> } />
+         onUpdateQuantity={handleUpdateQuantity}onRemoveUtem={handleRemoveItem} /> } />
+          <Route path='/checkout' element={<CheckoutPage cartItems={cartItems}
+          clearCart={clearCart}/>}/>
+          <Route path='/orders' element={<PurchareHistory />}/>
+
         </Routes>
         </main>
       <Footer/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ const Header = ()=>{
 
     return(
         <header className="bg-white shdov-md">
-         <div className='container mx-avto flex jistify-between items-center p-4'>
+         <div className='container mx-auto flex justify-between items-center p-4'>
             <Link to={"/"} className='text-2xl font-bold text-gray-600
             hover:text-gray-800'>
                 E-Commerce Shop
              </Link>
              <nav>
-            <Link to={"/cart"} className="text-gray-600 hover:text-gray-800">
-            Корзина
-            </Link>
+            <Link to={"/cart"} className='text-gray-600 hover:text-gray-800'>
+      Корзина
+     </Link>
              </nav>
             </div>   
         </header>
